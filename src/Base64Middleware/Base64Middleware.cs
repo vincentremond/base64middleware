f7fb3b851171376f5fc4c5e6bc608218b2d05595
Vincent Rémond <vincent.remond@gmail.com>
2020-02-06 11:38:20 +0100

    Initial commit

---
+++ src/Base64Middleware/Base64Middleware.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace Base64Middleware
+{
+    public class Base64Middleware
+    {
+        private readonly RequestDelegate _next;
+
+        public Base64Middleware(
+            RequestDelegate next)
+        {
+            if (next == null)
+            {
+                throw new ArgumentNullException(nameof(next));
+            }
+
+            _next = next;
+        }
+
+        /// <summary>
+        /// Invoke the middleware.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task InvokeAsync(
+            HttpContext context)
+        {
+            var originalBodyFeature = context.Features.Get<IHttpResponseBodyFeature>();
+            var compressionBody = new Base64Body(originalBodyFeature);
+            context.Features.Set<IHttpResponseBodyFeature>(compressionBody);
+
+            try
+            {
+                await _next(context);
+                await compressionBody.FinishEncryptionAsync();
+            }
+            finally
+            {
+                context.Features.Set(originalBodyFeature);
+            }
+        }
+    }
+}
